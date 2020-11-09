02948e6c0b8726116eb720fa921a70848e500641
Christian Heinemann <heinemann.christian@gmail.com>
2020-11-09 18:00:03 +0100

    considering token and timestamp for active simulations

--- src/php/api/getsimulation.php
+++ src/php/api/getsimulation.php
@@ -4,14 +4,29 @@
         exit();
     }
     if ($response= $db->query(
-        "SELECT sim.ID as id, sim.TOKEN as token, sim.NAME as simulationName, u.NAME as userName, sim.TIMESTEP as timestep, sim.SIZE_X as sizeX, sim.SIZE_Y as sizeY
+        "SELECT sim.ID as id, sim.TOKEN as token, sim.NAME as simulationName, u.NAME as userName, sim.TIMESTEP as timestep, sim.SIZE_X as sizeX, sim.SIZE_Y as sizeY, sim.LAST_UPDATE as lastUpdate
         FROM simulation sim, user u
         WHERE sim.USER_ID=u.ID")) {
 
         $result = array();
         
         while($obj = $response->fetch_object()){
-            $isActive = (bool)$obj->token;
+            $isActive = false;
+
+            if ($obj->token != null) {
+                $lastUpdateTimestamp = strtotime($obj->lastUpdate);
+
+                $date = new DateTime();
+                $currentTimestamp = $date->getTimestamp();
+
+                //invalidate token after 10 min
+                if ($currentTimestamp - $lastUpdateTimestamp > 60*10) {
+                    $db->query("UPDATE simulation SET TOKEN='' WHERE simulation.ID = " . $obj->id);
+                }
+                else {
+                    $isActive = true;
+                }
+            }
 
             $result[] = [
                 "id" => $obj->id, 
