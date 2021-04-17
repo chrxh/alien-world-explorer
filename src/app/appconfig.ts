e414fc1af5904827071dbc12ba274ba77a714824
Christian Heinemann <heinemann.christian@gmail.com>
2021-04-17 20:28:25 +0200

    php files for server removed

--- src/app/appconfig.ts
+++ src/app/appconfig.ts
@@ -1,5 +1,5 @@
 export class AppConfig {
-    public static readonly Address = "http://localhost/api/";
-//    public static readonly Address = "https://alien-project.org/world-explorer/api/";
+//    public static readonly Address = "http://localhost/api/";
+    public static readonly Address = "https://alien-project.org/world-explorer/api/";
  }
 
